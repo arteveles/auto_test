44bb9d575d3d79d4eaa06692d050227fc5cb6087
artem.shishlo <artem.shishlo@zyfra.com>
2021-09-26 06:25:12 -0700

    Создал класс и сделал из него фикстуру

--- 5_fixtures_return_values/conftest.py
+++ 5_fixtures_return_values/conftest.py
@@ -2,11 +2,9 @@ import pytest
 import random
 import math
 
-
 @pytest.fixture
 def fixture_return_rnd_number():
     return random.randint(1, 100)
-
 
 class TestClass:
 
@@ -17,4 +15,6 @@ class TestClass:
     def hello(self, name):
         return f"Hello, {name}"
 
-
+@pytest.fixture
+def fixture_return_class():
+    return TestClass(mod1=random, mod2=math)
