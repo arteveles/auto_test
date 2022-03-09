f79c06988803642473be181f7238971d59d51bb8
artem.shishlo <artem.shishlo@zyfra.com>
2022-03-09 06:10:38 -0800

    demo parser for https://stopgame.ru/

--- parser/test_parse.py
+++ parser/test_parse.py
@@ -1,4 +1,17 @@
-from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup as BS
 
-with open(f"{продумать как прокинуть сюда перебор всех страниц}") as file:
-    src = file.read()
+page = 1
+
+while True:
+    r = requests.get("https://stopgame.ru/review/new/izumitelno/p" + str(page))
+    html = BS(r.content, 'html.parser')
+    items = html.select(".items > .article-summary")
+
+    if (len(items)):
+        for el in items:
+            title = el.select('.caption > a')
+            print( title[0].text )
+        page += 1
+    else:
+        break
